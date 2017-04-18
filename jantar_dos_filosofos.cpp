d5eff5a6feeb32904fa989adc6c95b41c9d31179
Gabriell Araujo <ZK9RX6@users.noreply.github.com>
2017-04-17 22:18:16 -0300

    Update and rename jantar_dos_filosofos.c to jantar_dos_filosofos.cpp

---
+++ jantar_dos_filosofos.cpp
@@ -0,0 +1,45 @@
+
+#include <pthread.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+pthread_mutex_t* ponteiro_mutex_garfos;
+pthread_t* ponteiro_threads_filosofos;
+int numero_filosofos;
+
+void* funcao_filosofo(void* thread_argument)
+{
+	long filosofo_id = (long) thread_argument;
+
+
+	pthread_exit(NULL);
+} 
+
+int main(int argc, char* argv[])
+{
+	quantidade_filosofos = atoi(argv[1]);
+	
+	//alocando ponteiros
+	ponteiro_mutex_garfos = (pthread_mutex_t*) malloc(quantidade_filosofos * sizeof(pthread_mutex_t));
+	ponteiro_threads_filosofos = (pthread_t*) malloc(quantidade_filosofos * sizeof(pthread_t));	
+	
+	for(int i = 0; i < quantidade_filosofos; i++)
+	{			
+		//inicializando mutex
+		pthread_mutex_init(&ponteiro_mutex_garfos[i], NULL);
+	}
+	
+	for(long i = 0; i < quantidade_filosofos; i++)
+	{		
+		//criando filosofos
+		pthread_create(&ponteiro_threads_filosofos[i], NULL, funcao_filosofo, (void*)i);
+	}
+	
+	for(int i = 0; i < quantidade_filosofos; i++)
+	{			
+		pthread_join(ponteiro_threads_filosofos[i], NULL);
+	}
+	
+	
+}
